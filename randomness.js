74cba8e38c2f1f2910ff3a1d1081e07f1107d35b
aidanaASC6 <adofire11@gmail.com>
2019-07-11 11:58:38 -0400

    random commit

---
+++ randomness.js
@@ -0,0 +1,29 @@
+// GLOBAL VARIABLES //
+/////////////////////
+//const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const foods = [
+"pizza",
+"burgers",
+"rice and beans",
+"lasagna",
+"macaroni and cheese",
+"baked potatoes"
+];
+const students = [
+"Bob",
+"Martin",
+"Danny",
+"Madhi",
+"Harry",
+"Sean",
+"Robert"
+];
+
+//////////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////////
+randomNumber = Math.random();
+    console.log(randomNumber *20 + 1)
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+RandomSource(alphabet)
